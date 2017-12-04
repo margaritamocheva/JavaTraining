6dfc9c77efff900fb8bda011a70f4ba7f051ef6c
Unknown <mochevamargarita@gmail.com>
2017-12-04 17:44:26 +0200

    homeworkLastEdition

--- 01.FundamentalDataStructures/src/main/java/p1/TicketSystem.java
+++ 01.FundamentalDataStructures/src/main/java/p1/TicketSystem.java
@@ -1,10 +1,12 @@
 package p1;
 
 import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class TicketSystem {
 	public static void main(String[] args) {
-		ArrayDeque<Integer> tickets = new ArrayDeque<Integer>();
+		ArrayDeque<Integer> tickets = new ArrayDeque<Integer>(10);
 		tickets.offer(2);
 		tickets.offer(87);
 		tickets.offer(58);
@@ -15,5 +17,21 @@ public class TicketSystem {
 		tickets.offer(23);
 		tickets.offer(29);
 		tickets.offer(38);
+
+
+
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("If you want a ticket type \"give\" and if you don't want ticket type \"end\" ");
+		String comand;
+		comand=sc.next();
+		while(tickets.peek()!=null) {
+		if(comand=="give") {
+			System.out.println(tickets.poll());
+
+		}
+
+
 	}
+		}
 }
